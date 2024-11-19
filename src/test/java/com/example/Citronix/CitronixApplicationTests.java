4d8a4233faeff2db99c23248180c9a80bda78dbc
berghach <berghach.chemseddine@gmail.com>
2024-11-19 11:18:27 +0100

    first commit

---
+++ src/test/java/com/example/Citronix/CitronixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Citronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitronixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
